d072501b4d328daa621a7fd5a0bce02111d21f60
ederusena <eder.sena@live.com>
2021-05-05 21:41:36 -0300

    chore: Configurando webpack

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,8 +1,4 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
 
-function Wrappers() {
-  return (
-    <App></App>
-  )
-}
+render(<App />, document.getElementById('root'));
--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src, index.jsx'),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -14,7 +15,9 @@ module.exports = {
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+        }
       }
     ],
   }
