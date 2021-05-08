c4492634226c7054116d607591313703dc5899d7
ederusena <eder.sena@live.com>
2021-05-08 19:13:42 -0300

    chore: Style-loader Css-loader

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -30,7 +30,14 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: {
+          loader: ['style-loader', 'css-loader'],
+        }
+      },
     ],
   }
 }
