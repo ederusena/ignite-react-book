e0583984abdd46d532b1f50fcd895a7dd0161049
ederusena <eder.sena@live.com>
2021-05-28 16:49:35 -0300

    chore: useEffect

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss'
+
+// https://api.github.com/users/ederusena
 
 const repository = {
   name: 'ederusena',
@@ -7,9 +11,18 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRespositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/ederusena')
+      .then(response => response.json())
+      .then(data => setRespositories(data))
+  }, [])
+
   return (
     <section className='repository-list'>
       <h1>Lista de repository</h1>
+      
       <ul>
         <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
