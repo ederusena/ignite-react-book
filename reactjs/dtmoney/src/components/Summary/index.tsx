687b68b6541b613039c3391281b6d0a0f0f9ba3a
Eder Sena <eder.sena@live.com>
2021-09-01 09:02:43 -0300

    sumary

--- reactjs/dtmoney/src/components/Summary/index.tsx
+++ reactjs/dtmoney/src/components/Summary/index.tsx
@@ -1,5 +1,33 @@
+import incoming from '../../assets/income.svg';
+import outcoming from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from './styles'
+
 export function Summary() {
   return (
-
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incoming} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcoming} alt="Saidas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
   )
 }
