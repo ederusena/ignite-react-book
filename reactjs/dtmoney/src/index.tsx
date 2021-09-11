a26c797dac3fac6a93b3d01d2ab6f3f8722f6a5f
Eder Sena <eder.sena@live.com>
2021-09-10 22:45:02 -0300

    Configurando MirageJS e React-Modal

--- reactjs/dtmoney/src/index.tsx
+++ reactjs/dtmoney/src/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 
 import { App } from './App';
+
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Some transaction',
+          amount: 100,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Another transaction',
+          amount: 200,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
